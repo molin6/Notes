87c4aec9d88badda9c50528193f81b986c1c30b8
molin6 <isaiah90molina@gmail.com>
2023-01-04 13:38:23 -0600

    operators

---
+++ JS/Operators/operators.js
@@ -0,0 +1,40 @@
+// @ts-check
+//
+
+/**
+ * The day rate, given a rate per hour
+ *
+ * @param {number} ratePerHour
+ * @returns {number} the rate per day
+ */
+export function dayRate(ratePerHour) {
+    return 8 * ratePerHour;
+  }
+  
+  /**
+   * Calculates the number of days in a budget, rounded down
+   *
+   * @param {number} budget: the total budget
+   * @param {number} ratePerHour: the rate per hour
+   * @returns {number} the number of days
+   */
+  export function daysInBudget(budget, ratePerHour) {
+    const day = 8 * ratePerHour;
+    const yuh = budget / day;
+    return Math.floor(yuh)
+  }
+  
+  /**
+   * Calculates the discounted rate for large projects, rounded up
+   *
+   * @param {number} ratePerHour
+   * @param {number} numDays: number of days the project spans
+   * @param {number} discount: for example 20% written as 0.2
+   * @returns {number} the rounded up discounted rate
+   */
+  export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+    let months = Math.floor(numDays / 22);
+    let remaining_days = numDays % 22;
+    return Math.ceil((months * 22 * dayRate(ratePerHour) * (1 - discount)) + (remaining_days * dayRate(ratePerHour)));
+  }
+  
